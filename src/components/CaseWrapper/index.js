e95cacebc2731fa28cf186503772c7c12732e91c
sebastiangraz <graz@live.se>
2021-04-29 15:50:42 +0200

    V1

--- src/components/CaseWrapper/index.js
+++ src/components/CaseWrapper/index.js
@@ -6,6 +6,7 @@ import {
   useMotionValue,
   motionValue,
   transform,
+  useTransform,
 } from "framer-motion";
 const CaseWrapperContext = React.createContext(null);
 const useCaseWrapperContext = () => React.useContext(CaseWrapperContext);
@@ -23,6 +24,7 @@ const CaseWrapper = ({ children }) => {
     }
   );
 
+  //Get Children Heights//
   React.useEffect(() => {
     const ro = new ResizeObserver((entries) => {
       const childHeightArray = entries.map((entry) => {
@@ -36,44 +38,75 @@ const CaseWrapper = ({ children }) => {
     children.map((child) => ro.observe(child.ref.current));
   }, [children]);
 
+  //Get Children Positions//
   React.useEffect(() => {
-    const childHeight = state.childHeight;
     const childPositions = [];
-    const childsum = childHeight.reduce((acc, child) => {
+    state.childHeight.reduce((acc, child) => {
       childPositions.push(acc + child);
       return acc + child;
     }, 0);
-    const posValue = motionValue([]);
-    const updatePos = (v) => {
-      posValue.set(
-        childPositions.map((childPosition, i) => {
-          return transform(
-            v - childPosition + window.innerHeight + childHeight[i],
-            [0, childHeight[i]],
-            [0, 1]
-          );
-        })
-      );
-    };
-
     setCase({
       childPosition: childPositions,
-      scrollProgress: posValue,
-      childSummed: childsum,
+    });
+  }, [state.childHeight]);
+
+  //Send position to CHILD//
+  const posValue = useTransform(scrollY, (v) => {
+    const value = state.childPosition.forEach((element, i) => {
+      console.log(element[i]);
+      return v - element[i];
     });
 
-    const unsub = scrollY.onChange(updatePos);
+    return -value;
+  });
+
+  React.useEffect(() => {
+    setCase({
+      scrollProgress: posValue,
+    });
+  }, [posValue]);
+
+  React.useEffect(() => {
+    /* This useEffect handles getting the height of all children, 
+    and getting the artificial top position as if they were relative elements */
+
+    // const childHeight = state.childHeight;
+    // const childPositions = [];
+    // const childsum = childHeight.reduce((acc, child) => {
+    //   childPositions.push(acc + child);
+    //   return acc + child;
+    // }, 0);
+
+    /* UpdatePos function returns a number between 0-1 depending on the scroll progress
+    eg; [ 1,0.233,0,0 ], stored in a motion value to prevent re-render*/
+    // const posValue = motionValue([]);
+    // const updatePos = (v) => {
+    //   const pos = children.map((child, i) => {
+    //     return transform(v, [0, childHeight[i]], [0, 100]);
+    //   });
+    //   posValue.set(pos);
+    // };
+
+    // setCase({
+    //   childPosition: childPositions,
+    //   // scrollProgress: posValue,
+    //   childSummed: childsum,
+    // });
+
+    /* onChange is needed to update our MotionValues */
+    // const unsub = scrollY.onChange(updatePos);
 
     return () => {
-      unsub();
+      // unsub();
     };
-  }, [scrollY, state.childHeight]);
+  }, [state.childHeight]);
 
   return (
     <LazyMotion features={domAnimation} strict>
       <div
         style={{
-          height: state.childSummed,
+          height: 3000,
+          // height: state.childSummed,
         }}
       >
         <CaseWrapperContext.Provider value={state}>

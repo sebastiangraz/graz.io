c87a9afb960f195c9c7b30ca7e91efc63a12daa2
sebastiangraz <graz@live.se>
2021-05-11 01:42:35 +0200

    Big typography overhaul

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,7 +3,7 @@
 import { Text, jsx } from "theme-ui";
 import * as React from "react";
 
-export const List = ({ children, noBullets, numbered }) => {
+export const List = ({ children, noBullets }) => {
   return (
     <ul sx={{ padding: 0, listStyle: "none" }}>
       {React.Children.map(children || null, (child, i) => {
@@ -11,18 +11,20 @@ export const List = ({ children, noBullets, numbered }) => {
           <li
             key={i}
             sx={{
-              mb: 2,
+              mb: 1,
               position: "relative",
-              "&:before": {
-                content: `""`,
-                left: "-1em",
-                top: "0.8em",
-                position: "absolute",
-                borderRadius: "50%",
-                width: "3px",
-                height: "3px",
-                bg: "currentColor",
-              },
+              ...(!noBullets && {
+                "&:before": {
+                  content: `""`,
+                  left: "-1em",
+                  top: "0.8em",
+                  position: "absolute",
+                  borderRadius: "50%",
+                  width: "3px",
+                  height: "3px",
+                  bg: "currentColor",
+                },
+              }),
             }}
           >
             <Text>{child}</Text>

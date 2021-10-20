2400336bd155b6107e7c7d91013549864738fdd8
sebastiangraz <graz@live.se>
2021-10-20 10:40:30 +0200

    kill expand button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,7 @@
 import React from "react";
 import { Text, Grid } from "theme-ui";
 import { Logo, List } from "../";
-import {
-  m,
-  useViewportScroll,
-  useTransform,
-  AnimatePresence,
-  AnimateSharedLayout,
-} from "framer-motion";
+import { useViewportScroll, useTransform } from "framer-motion";
 
 const awards = [
   {
@@ -60,72 +54,8 @@ export const Home = ({ data }) => {
     mass: 0.1,
   });
 
-  const Button = () => {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const toggleOpen = () => setIsOpen(!isOpen);
-    return (
-      <>
-        <m.span
-          layout
-          onClick={toggleOpen}
-          style={{ backgroundColor: "currentColor" }}
-          sx={{
-            margin: 0,
-            width: "1em",
-            height: "1em",
-            verticalAlign: "middle",
-            borderRadius: "pill",
-            display: "inline-grid",
-            alignItems: "center",
-            justifyContent: "center",
-            position: "relative",
-            "&:hover": {
-              cursor: "pointer",
-            },
-            "&:before": {
-              content: "''",
-              backgroundColor: data.bg,
-              width: "10px",
-              height: "2px",
-              left: "calc(50% - 5px)",
-              position: "absolute",
-            },
-            "&:after": {
-              content: "''",
-              transition: "background .3s ease",
-              backgroundColor: isOpen ? "transparent" : data.bg,
-              height: "10px",
-              width: "2px",
-              left: "calc(50% - 1px)",
-              position: "absolute",
-            },
-          }}
-        ></m.span>
-
-        <AnimatePresence>
-          {isOpen && (
-            <m.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <Text mt={6}>
-                10 years of experience working with global clients with unique
-                challenges. With great gusto the studio specialises in unique
-                layouts, evoking colors, intricate interactions with a
-                detail-oriented finish. Are all attributes the studio strives
-                towards.
-              </Text>
-            </m.div>
-          )}
-        </AnimatePresence>
-      </>
-    );
-  };
-
   return (
-    <m.div
-      layout="position"
+    <div
       as={Grid}
       style={{ color, y }}
       sx={{
@@ -138,9 +68,9 @@ export const Home = ({ data }) => {
         rowGap: [8, 9, "16vh"],
         columnGap: 0,
         display: "grid",
-        alignItems: ["start", "start"],
+        alignItems: ["center", "start"],
         maxWidth: "2400px",
-        gridTemplateRows: "auto auto auto",
+        gridTemplateRows: "max-content max-content max-content",
         gridTemplateColumns: ["repeat(2, 1fr)", "repeat(12, 1fr)"],
         gridTemplateAreas: [
           //phone
@@ -168,97 +98,94 @@ export const Home = ({ data }) => {
         ],
       }}
     >
-      <AnimateSharedLayout>
-        <Logo
-          sx={{
-            fontVariationSettings: `"wght" 43`,
-            gridArea: "logo",
-            fontSize: 9,
-            display: "flex",
-          }}
-        />
+      <Logo
+        sx={{
+          fontVariationSettings: `"wght" 43`,
+          gridArea: "logo",
+          fontSize: 9,
+          display: "flex",
+        }}
+      />
 
-        <Text
-          variant="label"
-          sx={{ gridArea: "years", display: ["none", "block"] }}
-        >
-          Résumé
-        </Text>
-        <Text variant="label" sx={{ gridArea: "contact" }}>
-          Contact
-        </Text>
-        <m.div
-          layout="position"
+      <Text
+        variant="label"
+        sx={{ gridArea: "years", display: ["none", "block"] }}
+      >
+        Résumé
+      </Text>
+      <Text variant="label" sx={{ gridArea: "contact" }}>
+        Contact
+      </Text>
+      <div
+        sx={{
+          gridArea: "meta",
+          display: "grid",
+          rowGap: [7, null],
+          gridTemplateColumns: ["repeat(2, 1fr)", "repeat(3, 1fr)"],
+          width: "100%",
+        }}
+      >
+        <div>
+          <Text variant="label" mb={5}>
+            <Text variant="label" mb={4}>
+              Associations
+            </Text>
+            <List>
+              <>Husqvarna</>
+              <>M. Saatchi</>
+              <>Capchase</>
+              <>Framer</>
+              <>Canon</>
+              <>Volvo</>
+            </List>
+          </Text>
+        </div>
+        <div>
+          <Text variant="label" mb={5}>
+            <Text variant="label" mb={4}>
+              Expertise
+            </Text>
+            <List>
+              <>Empathic design</>
+              <>Design Systems</>
+              <>Art direction</>
+              <>Prototyping</>
+              <>Front-End</>
+              <>Branding</>
+            </List>
+          </Text>
+        </div>
+        <div>
+          <Text variant="label" mb={5}>
+            <Text variant="label" mb={4}>
+              Recognitions
+            </Text>
+            <List>
+              {awards.map((char) => (
+                <React.Fragment key={char.title}>
+                  {char.title} ·{" "}
+                  <Text sx={{ display: "inline" }} variant="caps">
+                    {char.count}×
+                  </Text>
+                </React.Fragment>
+              ))}
+            </List>
+          </Text>
+        </div>
+      </div>
+      <div sx={{ gridArea: "intro" }}>
+        <div
           sx={{
-            gridArea: "meta",
-            display: "grid",
-            rowGap: [7, null],
-            gridTemplateColumns: ["repeat(2, 1fr)", "repeat(3, 1fr)"],
-            width: "100%",
+            maxWidth: ["100%", "initial", null],
+            width: ["100%", 420, 360, 500],
           }}
         >
-          <div>
-            <Text variant="label" mb={5}>
-              <Text variant="label" mb={4}>
-                Associations
-              </Text>
-              <List>
-                <>Husqvarna</>
-                <>M. Saatchi</>
-                <>Capchase</>
-                <>Framer</>
-                <>Canon</>
-                <>Volvo</>
-              </List>
-            </Text>
-          </div>
-          <div>
-            <Text variant="label" mb={5}>
-              <Text variant="label" mb={4}>
-                Expertise
-              </Text>
-              <List>
-                <>Empathic design</>
-                <>Design Systems</>
-                <>Art direction</>
-                <>Prototyping</>
-                <>Front-End</>
-                <>Branding</>
-              </List>
-            </Text>
-          </div>
-          <div>
-            <Text variant="label" mb={5}>
-              <Text variant="label" mb={4}>
-                Recognitions
-              </Text>
-              <List>
-                {awards.map((char) => (
-                  <React.Fragment key={char.title}>
-                    {char.title} ·{" "}
-                    <Text sx={{ display: "inline" }} variant="caps">
-                      {char.count}×
-                    </Text>
-                  </React.Fragment>
-                ))}
-              </List>
-            </Text>
-          </div>
-        </m.div>
-        <div sx={{ gridArea: "intro" }}>
-          <m.div
-            sx={{
-              maxWidth: ["100%", "initial", null],
-              width: ["100%", 420, 360, 500],
-            }}
-          >
-            <Text variant="lead" mb={4}>
-              Sebastian Graz is a design studio for all things branding, digital
-              design & systems, served with a technological edge. <Button />
-            </Text>
-          </m.div>
+          <Text variant="lead" mb={4}>
+            Sebastian Graz is a design studio for all things branding, digital
+            design & systems, served with a technological edge.
+          </Text>
         </div>
-      </AnimateSharedLayout>
-    </m.div>
+      </div>
+    </div>
   );
 };

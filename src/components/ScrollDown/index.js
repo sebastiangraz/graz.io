c0bd41c128bbf1b9003ac8f3991bbea459ad7864
sebastiangraz <graz@live.se>
2021-11-01 16:14:22 +0100

    even nicer

--- src/components/ScrollDown/index.js
+++ src/components/ScrollDown/index.js
@@ -17,8 +17,8 @@ export const ScrollDown = () => {
       sx={{
         gridColumn: "2 / span 1",
         transition: "opacity 0.9s ease",
-        color: cases.get("home").bg,
-        backgroundColor: cases.get("home").color,
+        color: cases.get("home").color,
+        backgroundColor: lighten(cases.get("home").color, 0.78),
         position: "absolute",
         left: 0,
         top: ["-100px", "-200px", "-240px"],
@@ -31,7 +31,7 @@ export const ScrollDown = () => {
         svg: {
           position: "absolute",
           animationName: `${arrow}`,
-          animationDuration: "3s",
+          animationDuration: "4s",
           animationFillMode: "backwards",
           animationIterationCount: "infinite",
           animationTimingFunction: "cubic-bezier(.1, 0.82, 0.165, 1)",
@@ -44,6 +44,16 @@ export const ScrollDown = () => {
         viewBox="0 0 10 10"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M1 4L5 8L9 4" stroke="currentColor" strokeWidth="2" />
+      </svg>
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        sx={{ animationDelay: "2s" }}
       >
         <path d="M1 4L5 8L9 4" stroke="currentColor" strokeWidth="2" />
       </svg>

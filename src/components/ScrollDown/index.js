6620c6e71373f7cc592ac34f3b581c72c11693f0
sebastiangraz <graz@live.se>
2021-11-01 16:08:46 +0100

    ncier

--- src/components/ScrollDown/index.js
+++ src/components/ScrollDown/index.js
@@ -14,13 +14,11 @@ const arrow = keyframes`
 export const ScrollDown = () => {
   return (
     <div
-      style={{
-        color: cases.get("home").color,
-      }}
       sx={{
         gridColumn: "2 / span 1",
         transition: "opacity 0.9s ease",
-        backgroundColor: lighten(cases.get("home").color, 0.75),
+        color: cases.get("home").bg,
+        backgroundColor: cases.get("home").color,
         position: "absolute",
         left: 0,
         top: ["-100px", "-200px", "-240px"],
@@ -36,7 +34,7 @@ export const ScrollDown = () => {
           animationDuration: "3s",
           animationFillMode: "backwards",
           animationIterationCount: "infinite",
-          animationTimingFunction: "linear", // "cubic-bezier(.1, 0.82, 0.165, 1)",
+          animationTimingFunction: "cubic-bezier(.1, 0.82, 0.165, 1)",
         },
       }}
     >
@@ -47,19 +45,7 @@ export const ScrollDown = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M1 4L5 8L9 4" stroke="black" strokeWidth="2" />
-      </svg>
-      <svg
-        sx={{
-          animationDelay: "1.5s",
-        }}
-        width="10"
-        height="10"
-        viewBox="0 0 10 10"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M1 4L5 8L9 4" stroke="black" strokeWidth="2" />
+        <path d="M1 4L5 8L9 4" stroke="currentColor" strokeWidth="2" />
       </svg>
     </div>
   );

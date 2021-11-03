ef19d0a5e32116200890148666754e0174ae8fda
sebastiangraz <graz@live.se>
2021-11-03 15:22:32 +0100

    remove svg

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -98,16 +98,6 @@ export const ScrollToTop = () => {
             "& > .logo": { transition: "all 0.6s ease", minWidth: 0 },
           }}
         />
-        {/* <svg
-          width="10"
-          height="10"
-          viewBox="0 0 10 10"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          sx={{ transition: "all 0.2s ease" }}
-        >
-          <path d="M9 8L5 4L1 8" stroke="currentColor" strokeWidth="2" />
-        </svg> */}
       </div>
     </div>
   );

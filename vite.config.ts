e0fdac443361c61e5b46c66ac4c918a6ef28820f
sebastiangraz <graz@live.se>
2023-04-08 14:09:05 +0200

    add image compression

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
-
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+    }),
+  ],
 });

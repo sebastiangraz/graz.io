de5695be6a244a7360d4eb4d68eca864a294ac25
Sebastian Graz <graz@live.se>
2025-02-26 19:08:25 +0100

    progress

--- src/routes/articles/$articleId.tsx
+++ src/routes/articles/$articleId.tsx
@@ -3,7 +3,8 @@ import { ErrorComponent, createFileRoute } from "@tanstack/react-router";
 import { entryMeta } from "@/routes/articles";
 import type { ErrorComponentProps } from "@tanstack/react-router";
 import { Layout } from "@/components";
-
+import { Text } from "theme-ui";
+import { MDXProvider } from "@mdx-js/react";
 function PostErrorComponent({ error }: ErrorComponentProps) {
   return (
     <Layout>
@@ -15,7 +16,7 @@ function PostErrorComponent({ error }: ErrorComponentProps) {
 export const Route = createFileRoute("/articles/$articleId")({
   loader: async ({ params: { articleId } }) => {
     const article = entryMeta.find((article) => article.id === articleId);
-    console.log(entryMeta);
+    console.log();
     if (!article) {
       throw new Error("Article not found");
     }
@@ -35,8 +36,12 @@ function PostComponent() {
     <Layout>
       <div className="space-y-2">
         <h4 className="text-xl font-bold underline">{post.title}</h4>
-        <div className="text-sm">{post.body}</div>
+        <MDXProvider components={components}>{post.body}</MDXProvider>
       </div>
     </Layout>
   );
 }
+
+const components = {
+  h2: (props: any) => <Text {...props}>{props.children}</Text>,
+};

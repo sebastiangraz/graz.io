315da5aad3a7c758a1bb322ec1122b27746894be
Sebastian Graz <graz@live.se>
2026-04-17 01:08:46 +0200

    add new image comp

---
+++ src/pages/articles/figleaf-keyboard-part-one/ImageRow/ImageRow.tsx
@@ -0,0 +1,21 @@
+import { Children } from "react";
+import style from "./ImageRow.module.css";
+
+interface ImageRowProps {
+  children: React.ReactNode;
+}
+
+const ImageRow = ({ children }: ImageRowProps) => {
+  const count = Children.count(children);
+
+  return (
+    <div
+      className={style.row}
+      style={{ "--image-row-count": count } as React.CSSProperties}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { ImageRow };
